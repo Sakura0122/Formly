7180e6c94f695b9df2afd68835fbd96ea87a44fa
sakura <11854578@qq.com>
2026-03-22 16:07:27 +0800

    feat: 初始化前端项目

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { ref } from 'vue';
+
+ElMessage.success('13')
+const a=1;
+const num = ref(1)
+if(num){
+  
+}
+</script>
+
+<template>
+  <div class="w-5 h-5">
+    <Icon icon="ep:avatar" />
+  </div>
+  <ElButton>123</ElButton>
+</template>
+
+<style scoped></style>
